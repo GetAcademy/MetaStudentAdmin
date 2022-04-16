0b649ffe996f8f75bac0547ba84f8a2ef0f7db2c
GET Academy <terje@kolderup.net>
2022-04-16 19:40:03 +0200

    Initial commit

---
+++ MetaStudentAdmin/MetadataModel/Component.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetaStudentAdmin.MetadataModel
+{
+    internal class Component
+    {
+    }
+}
